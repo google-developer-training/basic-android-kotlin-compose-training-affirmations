94b43082a93b8c943d677de37ec11b49fed84e6f
schordas <schordas@google.com>
2022-06-13 13:24:54 -0700

    Initial starter code

---
+++ README.md
@@ -0,0 +1,18 @@
+Affirmations App - Starter Code
+================================
+
+Starter code for the Android Basics in Compose: Affirmations app.
+
+Pre-requisites
+--------------
+* Familiarity with Lists in Kotlin
+* Experience building layouts with Jetpack Compose
+* Experience running apps on a device or emulator
+
+
+Getting Started
+---------------
+1. Install Android Studio, if you don't already have it.
+2. Download the sample.
+3. Import the sample into Android Studio.
+4. Build and run the sample.
---
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.affirmations.data
+import com.example.affirmations.R
+
+/**
+ * [Datasource] generates a list of [Affirmation]
+ */
+class Datasource() {
+    /*fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation5, R.drawable.image5),
+            Affirmation(R.string.affirmation6, R.drawable.image6),
+            Affirmation(R.string.affirmation7, R.drawable.image7),
+            Affirmation(R.string.affirmation8, R.drawable.image8),
+            Affirmation(R.string.affirmation9, R.drawable.image9),
+            Affirmation(R.string.affirmation10, R.drawable.image10))
+    }*/
+}
