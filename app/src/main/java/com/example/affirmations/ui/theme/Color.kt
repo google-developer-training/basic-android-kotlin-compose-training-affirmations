790c5ab329ae80f1d139e7bbdd9456ac3249f107
John Shea <sheajohn@google.com>
2023-05-15 16:30:21 -0400

    Correct naming and M3 coding

--- app/src/main/java/com/example/affirmations/ui/theme/Color.kt
+++ app/src/main/java/com/example/affirmations/ui/theme/Color.kt
@@ -17,10 +17,10 @@ package com.example.affirmations.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_background = Color(0xFFFBFCFE)
+val md_theme_light_surfaceVariant = Color(0xFFE7E0EC)
+val md_theme_light_onSurfaceVariant = Color(0xFF49454f)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_dark_background = Color(0xFF191C1D)
+val md_theme_dark_surfaceVariant = Color(0xFF49454f)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCAC4D0)
