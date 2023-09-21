432d9fdfe870238f91dd8338d81ef808702c1112
schordas <schordas@google.com>
2023-09-21 13:03:45 -0700

    Update main branch with e2e

--- app/src/main/java/com/example/affirmations/ui/theme/Theme.kt
+++ app/src/main/java/com/example/affirmations/ui/theme/Theme.kt
@@ -15,9 +15,7 @@
  */
 package com.example.affirmations.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -25,12 +23,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     surfaceVariant = md_theme_dark_surfaceVariant,
