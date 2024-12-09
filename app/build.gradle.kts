e6f99a461af07028d0cebee4fa3764dde23abe24
Jose Alcérreca <jalc@google.com>
2024-12-09 16:59:33 +0000

    SDk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ plugins {
 
 android {
     namespace = "com.example.affirmations"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.affirmations"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
